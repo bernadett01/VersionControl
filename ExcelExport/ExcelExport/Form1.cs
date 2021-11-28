fdf8908b01a776fc12b280f160dc42d7a847bdf3
Bernadett Bihari <bernadett.bihari@stud.uni-corvinus.hu>
2021-11-28 22:45:45 +0100

    .

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -21,7 +21,6 @@ namespace ExcelExport
         Excel.Application xlApp;
         Excel.Workbook xlWb;
         Excel.Worksheet xlSheet;
-
         public Form1()
         {
             InitializeComponent();
@@ -31,20 +30,92 @@ namespace ExcelExport
 
         private void CreateExcel()
         {
-            xlApp = new Excel.Application();
-            xlWb = xlApp.Workbooks.Add();
-            xlWb = xlWb.ActiveSheet;
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWb = xlApp.Workbooks.Add();
+                xlWb = xlWb.ActiveSheet;
 
-            //CreateTable();
+                CreateTable();
 
-            xlApp.Visible = true;
-            xlApp.UserControl = true;
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine:{1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg);
+                xlWb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+
+            }
 
         }
 
-        private void LoadData()
+        private string CreateTable()
         {
-            Flats = context.Flats.ToList();
+            string[] headers = new string[] {
+
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+
+            for (int i = 0; i <= headers.Length; i++)
+            {
+                xlSheet.Cells[1, 1] = headers[i - 1];
+
+                object[,] values = new object[Flats.Count, headers.Length];
+                int counter = 0;
+                foreach (Flat item in Flats)
+                {
+                    values[counter, 0] = item.Code;
+                    values[counter, 1] = item.Vendor;
+                    values[counter, 2] = item.Side;
+                    values[counter, 3] = item.District;
+                    values[counter, 4] = item.Elevator;
+                    values[counter, 5] = item.NumberOfRooms;
+                    values[counter, 6] = item.FloorArea;
+                    values[counter, 7] = item.Price;
+                    values[counter, 8] = "";
+                    counter++;
+
+
+                }
+
+            }
+
+            private void LoadData()
+            {
+                Flats = context.Flats.ToList();
+            }
+
+            private string GetCell(int x, int y);
+            {
+                string ExcelCoordinate = "";
+                int dividend = y;
+                int modulo;
+
+                while (dividend > 0)
+                {
+
+                    modulo = (dividend - 1) % 26;
+                    ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                    dividend = (int)((dividend - modulo) / 26);
+
+                }
+                ExcelCoordinate += x.ToString();
+                return ExcelCoordinate;
+            }
         }
-    }
+    } 
 }
