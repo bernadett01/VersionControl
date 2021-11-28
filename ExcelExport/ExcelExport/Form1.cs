1551d2f4b72e5a00d688f52855f5073f35d1098c
Bernadett Bihari <bernadett.bihari@stud.uni-corvinus.hu>
2021-11-28 22:13:34 +0100

    .

---
+++ ExcelExport/ExcelExport/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace ExcelExport
+{
+    public partial class Form1 : Form
+    {
+
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlSheet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+            CreateExcel();
+        }
+
+        private void CreateExcel()
+        {
+            xlApp = new Excel.Application();
+            xlWb = xlApp.Workbooks.Add();
+            xlWb = xlWb.ActiveSheet;
+
+            //CreateTable();
+
+            xlApp.Visible = true;
+            xlApp.UserControl = true;
+
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+    }
+}
